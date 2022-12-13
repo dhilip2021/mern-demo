d95732b4d875ae60510d505c219b43dd2666fb9e
sabari <sabari>
2022-12-13 15:35:05 +0530

    mongo db connected

--- src/index.server.js
+++ src/index.server.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app= express();
 const env = require("dotenv");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 
 env.config();
 app.use(cors())
 app.use(express.json());
 
-
-
+const connectionURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.7phnzog.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+mongoose.set('strictQuery', false);
+mongoose.connect(connectionURL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+).then(console.log("DB Conncted"))
 
 
 
